0d0a96c36bb5a13b323d19e82f3b6c0af339b125
SOFTWARE-SV\zar <zartablet2022@gmail.com>
2023-08-07 18:19:47 +0330

    game

---
+++ questionnaire/src/main/java/com/msa/questionnaire/data/AllWordsModel.kt
@@ -0,0 +1,23 @@
+package com.msa.questionnaire.data
+
+const val MAX_NO_OF_WORDS = 10
+const val SCORE_INCREASE = 20
+
+// Set with all the words for the Game
+val allWords: Set<String> =
+    setOf(
+        "animal",
+        "auto",
+        "anecdote",
+        "alphabet",
+        "all",
+        "awesome",
+        "arise",
+        "balloon",
+        "basket",
+        "bench",
+        // ...
+        "zoology",
+        "zone",
+        "zeal"
+    )
