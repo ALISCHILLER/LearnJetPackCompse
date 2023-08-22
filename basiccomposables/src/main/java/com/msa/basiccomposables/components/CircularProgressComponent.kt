b32c302a200d933ef50fdb3b3cbba4ca704e851e
SOFTWARE-SV\zar <zartablet2022@gmail.com>
2023-08-22 18:54:36 +0330

    CircularProgress

---
+++ basiccomposables/src/main/java/com/msa/basiccomposables/components/CircularProgressComponent.kt
@@ -0,0 +1,18 @@
+package com.msa.basiccomposables.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun CircularProgressTest(){
+
+
+}
+
+
+@Composable
+@Preview
+fun CircularProgressTestPreview(){
+    CircularProgressTest()
+}
